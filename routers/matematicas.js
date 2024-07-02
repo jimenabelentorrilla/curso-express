a00a4358e5f43f810309a2f19c8dea2a6e4db433
Jimena Belen <jiment4@hotmail.com>
2024-07-02 01:03:03 -0300

    [add] metodos http

--- routers/matematicas.js
+++ routers/matematicas.js
@@ -5,13 +5,16 @@ const {matematicas} = require('../datos/cursos.js').infoCursos;
 
 const routerMatematicas = express.Router();
 
+  // Middleware
+  routerMatematicas.use(express.json());
+
 // Parametros de ruta (o URL)
 
 // Cursos de matematicas
     
 routerMatematicas.get('/', (req, res) => {
   /*para hacer el codigo mas conciso en lugar de hacer notacion de punto aca, asignamos directamente matematicas en lugar de infoCurso.matematicas*/
-  res.send(JSON.stringify(matematicas));
+  res.json(matematicas); /**quite JSON.STRINGIFY y modifique send por json*/
 });
 
 routerMatematicas.get('/:tema', (req, res) => {
@@ -20,10 +23,84 @@ routerMatematicas.get('/:tema', (req, res) => {
 
   if (resultados.length === 0) {
     return res.status(404).send(`No se encontraron cursos de ${tema}.`);
+    /*** return res.status(404).end();  Método para enviar una rta. vacia*/
   }
 
-  res.send(JSON.stringify(resultados));
+  res.json(resultados); /**quite JSON.STRINGIFY y modifique send por json*/
 });
+
+// Si quiero filtrar por dos parametros
+
+routerMatematicas.get('/:tema/:nivel', (req, res) => {
+  const tema = req.params.tema;
+  const nivel = req.params.nivel;
+    
+  const resultados = programacion.filter(curso => curso.tema === tema && curso.nivel === nivel);
+     
+  if (resultados. length === 0){
+    return res.status(404).send(`No se encontraron cursos de ${tema} de nivel ${nivel}.`);
+    /*** return res.status(404).end();  Método para enviar una rta. vacia*/
+  }
+    
+  if (req.query.ordenar === 'vistas') {
+    return res.send(resultados.sort((a, b) => b.vistas - a.vistas)); /**quite JSON.STRINGIFY*/
+  }
+    res.json(resultados); /**quite JSON.STRINGIFY y modifique send por json*/
+});
+
+
+
+
+// POST
+routerMatematicas.post('/', (req , res) => {
+  let cursoNuevo = req.body; 
+    matematicas.push(cursoNuevo); 
+    res.json(matematicas);
+});
+
+
+
+// PUT
+routerMatematicas.put('/:id', (req, res) => {
+  const cursoActualizado = req.body; 
+  const id = req.params.id; 
+  
+  const indice = matematicas.findIndex(curso => curso.id == id); 
+  
+  if (indice >= 0) {
+    matematicas[indice] = cursoActualizado; 
+  }
+  res.json(matematicas);
+});
+
+//PATCH
+routerMatematicas.patch('/:id', (req, res) => {
+  const infoActualizada = req.body;
+  const id = req.params.id; 
+  
+  const indice = matematicas.findIndex(curso => curso.id == id);
+  
+  if (indice >= 0) {
+    const cursoAModificar = matematicas[indice];
+   
+    Object.assign(cursoAModificar, infoActualizada);
+  }
+  res.json(matematicas); 
+});
+
+//DELETE
+routerMatematicas.delete('/:id', (req, res) => {
+  const id = req.params.id;
+    const indice = matematicas.findIndex(curso => curso.id = id);
+    
+    if (indice >= 0){
+      matematicas.splice(indice, 1); 
+    }
+    res.json(matematicas);
+});
+
+
+
 
 module.exports = routerMatematicas;
 
--- routers/programacion.js
+++ routers/programacion.js
@@ -5,12 +5,15 @@ const {programacion} = require('../datos/cursos.js').infoCursos;
 
 const routerProgramacion = express.Router();
 
+  // Middleware
+  routerProgramacion.use(express.json());
+
 // Parametros de ruta (o URL)
 
 //Cursos de programacion
 
 routerProgramacion.get('/', (req, res) => {
-    res.send(JSON.stringify(programacion ));
+    res.json(programacion); /**quite JSON.STRINGIFY y modifique send por json*/
 });
 
 routerProgramacion.get('/:lenguaje', (req, res) => {
@@ -19,9 +22,10 @@ routerProgramacion.get('/:lenguaje', (req, res) => {
 
     if (resultados.length === 0) {
         return res.status(404).send(`No se encontraron cursos de ${lenguaje}.`);
+      /*** return res.status(404).end();  Método para enviar una rta. vacia*/
     }
 
-    res.send(JSON.stringify(resultados));
+    res.json(resultados); /**quite JSON.STRINGIFY y modifique send por json*/
 });
 
 // Si quiero filtrar por dos parametros
@@ -33,15 +37,61 @@ routerProgramacion.get('/:lenguaje/:nivel', (req, res) => {
   const resultados = programacion.filter(curso => curso.lenguaje === lenguaje && curso.nivel === nivel);
      
   if (resultados. length === 0){
-    return res.status(404).send(`No se encontraron cursos de ${lenguaje} de nivel ${nivel}.`)
+    return res.status(404).send(`No se encontraron cursos de ${lenguaje} de nivel ${nivel}.`);
+    /*** return res.status(404).end();  Método para enviar una rta. vacia*/
   }
     
   if (req.query.ordenar === 'vistas') {
-    return res.send(JSON.stringify(resultados.sort((a, b) => b.vistas - a.vistas)));
+    return res.send(resultados.sort((a, b) => b.vistas - a.vistas)); /**quite JSON.STRINGIFY*/
   }
-    res.send(JSON.stringify(resultados));
-
-  res.send(JSON.stringify(resultados));
+    res.json(resultados); /**quite JSON.STRINGIFY y modifique send por json*/
 });
+
+  // POST
+  routerProgramacion.post('/', (req, res) => {
+    let cursoNuevo = req.body; 
+    programacion.push(cursoNuevo); 
+    res.json(programacion); /**quite JSON.STRINGIFY y modifique send por json*/
+  });
+
+  // PUT
+  routerProgramacion.put('/:id', (req, res) => { 
+    const cursoActualizado = req.body; 
+    const id = req.params.id; 
+    
+    const indice = programacion.findIndex(curso => curso.id == id); 
+    
+    if (indice >= 0) {
+      programacion[indice] = cursoActualizado; 
+    }
+    res.json(programacion); /**quite JSON.STRINGIFY y modifique send por json*/
+  }); 
+
+  //PATCH
+  routerProgramacion.patch('/:id', (req, res) => {
+    const infoActualizada = req.body;
+    const id = req.params.id; 
+    
+    const indice = programacion.findIndex(curso => curso.id == id);
+    
+    if (indice >= 0) {
+	    const cursoAModificar = programacion[indice];
+	   
+	    Object.assign(cursoAModificar, infoActualizada);
+    }
+    res.json(programacion); /**quite JSON.STRINGIFY y modifique send por json*/
+  });
+
+  //DELETE
+
+  routerProgramacion.delete('/:id', (req, res) => {
+    const id = req.params.id;
+    const indice = programacion.findIndex(curso => curso.id = id);
+    
+    if (indice >= 0){
+      programacion.splice(indice, 1); 
+    }
+    res.json(programacion); /**quite JSON.STRINGIFY y modifique send por json*/
+  });
 
 module.exports = routerProgramacion;
